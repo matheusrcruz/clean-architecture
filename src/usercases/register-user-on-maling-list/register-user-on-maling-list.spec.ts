0a714e66bd589b7671353925b5c98d5393cea519
matheusrcruz <git.dev@outlook.com>
2022-01-18 06:11:15 -0300

    feat: user data interface

---
+++ src/usercases/register-user-on-maling-list/register-user-on-maling-list.spec.ts
@@ -0,0 +1,15 @@
+import { UserData } from './UserData'
+
+describe('Register user on maling list user case', () => {
+  test('should add user with complete data to maling list', async () => {
+    const users: UserData[] = []
+    console.log(users)
+    // const repo: UserRepository = new inMermoryRepository(users)
+    // const useCase: RegisteUserOnMalingList = new RegisteUserOnMalingList(repo)
+    // const name = 'any_name'
+    // const email = 'any@email.com'
+    // const response = await useCase.registeUserOnMalingList({ name, email })
+    // const user = repo.findUserByEmail('any@email.com')
+    // expect((await user).name).toBe('any_name')
+  })
+})
